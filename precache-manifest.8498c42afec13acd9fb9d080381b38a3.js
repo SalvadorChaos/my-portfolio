231247169b409f193556df1a7a116cef9be221d1
Salvador Martinez Jr <salvadormartinez022693@gmail.com>
2019-11-13 16:22:58 -0600

    Updates

---
+++ precache-manifest.8498c42afec13acd9fb9d080381b38a3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7fcf8f8b73676539434b62c4fb5ffb0",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "bcc9f4524cba56db76d7",
+    "url": "/my-portfolio/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "1798b6f72a81d88f4596",
+    "url": "/my-portfolio/static/css/main.351294fd.chunk.css"
+  },
+  {
+    "revision": "bcc9f4524cba56db76d7",
+    "url": "/my-portfolio/static/js/2.93cb5b34.chunk.js"
+  },
+  {
+    "revision": "1798b6f72a81d88f4596",
+    "url": "/my-portfolio/static/js/main.467ff6e7.chunk.js"
+  },
+  {
+    "revision": "d30fa1a6b67316ef599f",
+    "url": "/my-portfolio/static/js/runtime~main.8e6d56fe.js"
+  },
+  {
+    "revision": "3103c926b471323a52004181690ec6c3",
+    "url": "/my-portfolio/static/media/dicechallengepic.3103c926.png"
+  },
+  {
+    "revision": "40262f33eedee00a1524e4b85df5eef6",
+    "url": "/my-portfolio/static/media/drumkitpic.40262f33.png"
+  },
+  {
+    "revision": "9379dd4c8e40b80d6c1fdea5411cf587",
+    "url": "/my-portfolio/static/media/expensifypic.9379dd4c.png"
+  },
+  {
+    "revision": "6334b414843f78303c758d70ebb56fa9",
+    "url": "/my-portfolio/static/media/indecisionpic.6334b414.png"
+  },
+  {
+    "revision": "f3305970f16aaa5fdddd009d46820ad4",
+    "url": "/my-portfolio/static/media/profilepic.f3305970.jpg"
+  },
+  {
+    "revision": "d779abe779b13b013e4ecec0681f4c1d",
+    "url": "/my-portfolio/static/media/secretspic.d779abe7.png"
+  },
+  {
+    "revision": "fedbba23fc5afbba4a6181ab99ecbfbc",
+    "url": "/my-portfolio/static/media/simonsayspic.fedbba23.png"
+  }
+]);
